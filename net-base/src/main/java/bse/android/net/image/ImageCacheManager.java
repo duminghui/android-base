96ce92b2a9df5650ada968e21e4d78ef34497126
duminghui <duminghui@139.com>
2014-10-13 00:04:08 +0800

    1、添加网络处理模块

---
+++ net-base/src/main/java/bse/android/net/image/ImageCacheManager.java
@@ -0,0 +1,35 @@
+package bse.android.net.image;
+
+import com.android.volley.toolbox.ImageLoader;
+
+import base.android.net.NetReqManager;
+
+/**
+ * 图片缓存管理
+ * Created by dumh on 14/10/12.
+ */
+public class ImageCacheManager {
+    private static final ImageCacheManager instance = new ImageCacheManager();
+
+    private ImageLoader imageLoader;
+
+    private ImageLoader.ImageCache imageCache;
+
+    private ImageCacheManager() {
+
+    }
+
+    public static ImageCacheManager getInstance() {
+        return instance;
+    }
+
+    public void init(int cacheSizeLimit) {
+        imageCache = new BitmapLruImageCache(cacheSizeLimit);
+        imageLoader = new ImageLoader(NetReqManager.getRequestQueue(), imageCache);
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+
+}
